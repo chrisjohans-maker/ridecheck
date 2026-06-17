c0fb6fd24abaaf15115476f8fd015e3985f96a06
chrisjohans-maker <chris.johans@tastytrade.com>
2026-06-16 22:45:32 -0500

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-// Build: Jun 17 2026 03:38 UTC
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', e => {
-  if (e.request.mode === 'navigate') {
-    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-  }
-});
