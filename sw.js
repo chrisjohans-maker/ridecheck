59bd0cb2cfd750cf8928aedeb4333f811cd0c281
chrisjohans-maker <chris.johans@tastytrade.com>
2026-06-26 15:43:30 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+// Build: Jun 26 2026 21:55 UTC
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', e => {if(e.request.mode==='navigate'){e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))}});
